851fd998a15ab0cace6fdae87f1e625f918bd9a4
bruzhmelev <bruzhmelev@gmail.com>
2019-08-24 13:27:06 +0300

    part 1

---
+++ 1/Send/Send.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() {HostName = "localhost", UserName = "rabbitmq", Password = "rabbitmq"};
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    string message = "Hello, World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+
+            System.Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
